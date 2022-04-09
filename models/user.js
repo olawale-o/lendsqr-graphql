ee11388fb7ca0f75999196eb8fe5e751030a007a
olawale-o <omoogunolawale@gmail.com>
2022-04-09 11:44:04 +0100

    Add createUser method on user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,23 @@
 const db = require('../database');
 
 module.exports = {
+  createUser: async (credentials) => {
+    const { email, password, firstName, lastName, accountNo } = credentials;
+    const user = await db('user').insert({
+        email,
+        password,
+        first_name: firstName,
+        last_name: lastName,
+        account_no: accountNo,
+    }).then(id => db('user').where({ id }).first('id', 'email', 'first_name', 'last_name', 'balance', 'account_no'));
+    return user;
+  },
+
   findBy: async (credentials) => {
     const user = await db('user').where(credentials).first('id', 'email', 'first_name', 'last_name', 'balance', 'password', 'account_no');
     return user;
   },
+
   getUsers: async () => {
     try {
         const users = await db.select('id', 'first_name', 'last_name', 'email', 'balance', 'created_at', 'updated_at').from('user');
