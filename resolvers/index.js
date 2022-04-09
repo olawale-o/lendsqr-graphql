8e5b0709e4646b512aa353fd2e8dd7c71bf8deaf
olawale-o <omoogunolawale@gmail.com>
2022-04-09 11:44:54 +0100

    Resolve register in mutation

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,16 +1,21 @@
+const { env: { JWT_SECRET } } = require('../constants');
+const jwt = require('jsonwebtoken');
+const UserController = require('../controllers/users_controller');
 const resolvers = {
   Query: {
-    users: async (_, __, { dataSources }) => dataSources.UserService.allUsers(),
+    users: async (parent, args, { dataSources }, context) => dataSources.UserService.allUsers(),
   },
 
   Mutation: {
     login: async (parent, { input }, context) => {
-      const { email, password } = input;
-      const obj = await context.authenticate("graphql-local", {
-        email,
-        password
-      });
-      return { obj };
+      const obj = await UserController.authenticateUser(input);
+      const token = jwt.sign({ user: obj.user }, JWT_SECRET, { expiresIn: '1y' })
+      return { user: obj.user, token };
+    },
+    register:  async (parent, { input }, context) => {
+      const obj = await UserController.createUser(input);
+      const token = jwt.sign({ user: obj.user }, JWT_SECRET, { expiresIn: '1y' })
+      return { user: obj.user, token };
     },
   }
 };
